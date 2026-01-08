0703f6624a0e26006efc417976cac78daa18efa6
Your Name <you@example.com>
2026-01-08 08:45:58 +0530

    initial commit with Login Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Components/Login.jsx'
+
+function App() {
+
+  return (
+    <>  
+        <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import "./Login.css"
+import logo from "/BXP-Logo.webp"
+export default function Login() {
+    let [email, setEmail] = useState(null)
+    let [pass, setPass] = useState(null)
+    
+    const handleEmail = (e) => { 
+        setEmail(e.target.value)
+    }
+    const handlePass = (e) => {
+        setPass(e.target.value)
+        localStorage.name = e.target.value
+    }
+
+    return (
+        <>  
+            <div className="flex items-center justify-center h-screen">
+                <div className="w-[80vw]  h-[80vh] flex">
+                    <div className=" w-full lg:w-1/2 flex items-center bg-blue-900 justify-center border-r border-white rounded-lg shadow-[0_4px_6px_rgba(0,0,0,1)]">
+                        <div>
+                            <h2 className="text-white text-xl">Dashboard</h2>
+                            <p className="text-gray-300">Enter your credential to login to the portal</p><br/>
+                            <label className="text-gray-400">Email:</label><br/>
+                            <input type="email" id="email" name="email" className="border-0 border-b-2 border-white focus:outline-none" onChange={(e)=>handleEmail(e)}/><br/>
+                            <label className="text-gray-400">Password:</label><br/>
+                            <input type="password" id="password" name="password" className="border-b-2 border-white focus:outline-none" onChange={(e)=>handlePass(e)}/><br/>
+                        </div>
+                        
+                    </div>
+                    <div className="w-1/2 flex items-center justify-center bg-white hidden lg:flex rounded-lg shadow-[0_4px_2px_rgba(0,0,0,0.4)]">
+                        <img src={logo} className="w-80 h-auto" alt="Logo"></img>
+                    </div>
+                </div>
+            </div>
+        </>
+        )
+}
